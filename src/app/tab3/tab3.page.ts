6f846af9956a589d9969b302b326b53dde5f4709
Antemp12 <tomas.amorim@ipvc.pt>
2025-05-31 19:19:09 +0100

    Tab3 e mais algumas coisas mudadas

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,17 +1,39 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, PermissionStatus } from '@capacitor/camera';
 import { IonicModule } from '@ionic/angular';
 import { UserService } from '../services/user.service';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { CommonModule } from '@angular/common'; // <-- Add this import
 
 @Component({
   selector: 'app-tab3',
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponentModule],
+  imports: [IonicModule, ExploreContainerComponentModule, CommonModule], // <-- Add this line
   templateUrl: './tab3.page.html',
   styleUrls: ['./tab3.page.scss'],
 })
 export class Tab3Page {
-
+  image: string | undefined;
 
   constructor(private userService: UserService) {}
+
+  async ionViewDidEnter() {
+    // Ask for camera permission
+    const permission: PermissionStatus = await Camera.requestPermissions({ permissions: ['camera'] });
+    if (permission.camera === 'granted') {
+      this.openCamera();
+    } else {
+      alert('Camera permission is required to use this feature.');
+    }
+  }
+
+  async openCamera() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    });
+    this.image = image.dataUrl;
+  }
 }
